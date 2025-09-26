36f9a2aa56b4f90f44c5f7328d9ebded87141e22
Burrpiee <135572400+Burrpiee@users.noreply.github.com>
2025-09-27 01:19:40 +0800

    Saving progress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+
+async function getWeatherData (location, unit) {
+    const apiKey = '8TZ85U7XYQW26HBA9SJCEBTZV';
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?key=${apiKey}&unitGroup=${unit}&contentType=json`
+
+    const response = await fetch(url);
+    const weatherData = await response.json();
+
+    console.log(weatherData);
+
+    // Objects with data needed
+    const currentConditions = weatherData.currentConditions;
+    // Retrieving important data
+    const temp = currentConditions.temp;
+    const conditions = currentConditions.conditions;
+    const humidity = currentConditions.humidity;
+
+    console.log(temp, conditions, humidity);
+}
+
+
+const weatherData = getWeatherData(`singapore`, `metric`);
